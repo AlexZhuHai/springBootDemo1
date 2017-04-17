1384520f74ccac2a7c9524914493d7fc4df46834
Administrator <Administrator@SC-201701160933>
2017-04-17 13:51:51 +0800

    alex

---
+++ springBootDemo1/src/main/java/com/springBootDemo1/SpringBootDemo1Application.java
@@ -0,0 +1,12 @@
+package com.springBootDemo1;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableAutoConfiguration
+@SpringBootApplication
+public class SpringBootDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemo1Application.class, args);
+	}
+}
